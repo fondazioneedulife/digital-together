75ac83a46153f0749751fef94462a5c2fe8c1a99
pietrospoletti <spoletti14@gmail.com>
2022-02-28 12:20:09 +0100

    detail map

--- client/src/components/Detail.vue
+++ client/src/components/Detail.vue
@@ -11,7 +11,7 @@
         <div class="justify-between px-5 py-6 text-xl text-left">
           <span class="text-3xl font-bold">{{ corso.nome }}</span> <br />
           <p>
-            data inizio: {{ corso.data_inizio }}  / data fine:
+            data inizio: {{ corso.data_inizio }} / data fine:
             {{ corso.data_fine }}
           </p>
         </div>
@@ -26,19 +26,85 @@
           <p>email: {{ palestra.email }}</p>
           <p>indirizzo: {{ palestra.indirizzo }}</p>
         </div>
+        <ol-map
+          v-if="this.lat && this.long"
+          ref="map"
+          :loadTilesWhileAnimating="true"
+          :loadTilesWhileInteracting="true"
+          style="height: 20rem; width: 60vw"
+        >
+          <ol-view
+            ref="view"
+            :center="center"
+            :rotation="rotation"
+            :zoom="zoom"
+            :projection="projection"
+          />
+
+          <ol-fullscreen-control
+            v-if="fullscreencontrol"
+            tipLabel="full-screen"
+          />
+
+          <ol-tile-layer>
+            <ol-source-osm />
+          </ol-tile-layer>
+
+          <ol-vector-layer>
+            <ol-source-vector>
+              <ol-feature>
+                <ol-geom-multi-point
+                  :coordinates="[[this.lat, this.long]]"
+                ></ol-geom-multi-point>
+                <ol-style>
+                  <ol-style-icon :src="markerIcon" :scale="1"></ol-style-icon>
+                </ol-style>
+              </ol-feature>
+            </ol-source-vector>
+          </ol-vector-layer>
+        </ol-map>
       </div>
     </div>
   </div>
 </template>
 <script>
 import axios from "axios";
+import { ref, inject } from "vue";
+import markerIcon from "@/assets/PointMap.svg";
 export default {
+  setup() {
+    const center = ref([11, 45.44]);
+    const projection = ref("EPSG:4326");
+    const zoom = ref(11);
+    const rotation = ref(0);
+    const coordinates = ref([
+      [0, 0],
+    ]);
+
+    const format = inject('ol-format');
+    console.log(format);
+
+    const geoJson = new format.GeoJSON();
+
+    return {
+      center,
+      projection,
+      zoom,
+      rotation,
+      geoJson,
+      markerIcon,
+      coordinates,
+    };
+  },
   data() {
     return {
       corso: {},
       categoria: {},
       palestra: {},
-      error: [],
+      errors: [],
+      fullscreencontrol: true,
+      lat: "",
+      long: "",
     };
   },
   async mounted() {
@@ -49,6 +115,10 @@ export default {
         this.corso = response.data;
         this.categoria = response.data.idCategory;
         this.palestra = response.data.idGym;
+        this.lat = this.palestra.lat;
+        this.long = this.palestra.long;
+        console.log(this.lat, this.long);
+        this.mapsetup();
       })
       .catch((error) => {
         if (error.response) {
